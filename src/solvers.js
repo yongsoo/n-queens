614a44b4956dd3ad505fd6954b94f7d110a805cc
Hack Reactor Students <students@hackreactor.com>
2013-11-20 16:22:22 -0800

    Finds a valid solution for n of 1-8 for rook

--- src/solvers.js
+++ src/solvers.js
@@ -13,7 +13,41 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
+  n = n || 1;
+  var solution = undefined;
+  var rowIndex = 0;
+  var colIndex = 0;
+
+  var board = new Board({'n': n});
+
+  var recur = function() {
+    if(rowIndex > n - 1) {
+      return;
+    }
+
+    board.togglePiece(rowIndex, colIndex);
+
+    if(board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
+      // remove the piece;
+      board.togglePiece(rowIndex, colIndex);
+
+      if(colIndex <= n - 1) {
+        colIndex++;
+        recur();
+      }
+    }
+
+    // go to next row
+    rowIndex++;
+    colIndex = 0;
+
+    recur();
+
+  }
+
+  recur();
+
+  solution = board.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
