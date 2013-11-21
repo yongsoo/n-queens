e8d74474c995aa34cdf8c7a155b8e1e5396b985b
Hack Reactor Students <students@hackreactor.com>
2013-11-21 11:44:20 -0800

    found single rook solution

--- src/solvers.js
+++ src/solvers.js
@@ -12,56 +12,20 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-window.findNRooksSolution = function(n, colIndex, rowIndex){
+window.findNRooksSolution = function(n){
   n = n || 1;
-  colIndex = colIndex || 0;
-  rowIndex = rowIndex || 0;
-  var solution = undefined;
-  var piecesPlaced = 0;
 
   var board = new Board({'n': n});
-
   console.log(n);
 
-  var recur = function() {
-    // if(rowIndex > n - 1) {
-    if(piecesPlaced === n) {
-      return;
-    }
-
-    board.togglePiece(rowIndex, colIndex);
-
-    if(board.hasAnyRooksConflicts()) {
-      // remove the piece;
+  _.each(board.rows(), function(row, rowIndex) {
+    _.each(row, function(col, colIndex) {
       board.togglePiece(rowIndex, colIndex);
-
-      // move to next col
-      if(colIndex <= n - 1) {
-        colIndex++;
-        recur();
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(rowIndex, colIndex);
       }
-    }
-
-    piecesPlaced++;
-
-    // go to next row
-    rowIndex++;
-    colIndex = 0;
-
-    // If at the last row, start from top again
-    // if(rowIndex > n - 1) {
-      // rowIndex = 0;
-    // }
-
-    // if(piecesPlaced === 1 && n !== 1) {
-      // rowIndex = 0;
-    // }
-
-    recur();
-
-  }
-
-  recur();
+    });
+  });
 
   solution = board.rows();
 
@@ -75,31 +39,8 @@ window.findNRooksSolution = function(n, colIndex, rowIndex){
 window.countNRooksSolutions = function(n){
   // debugger;
   n = n || 1;
-  var solutionCount = 0;
-  var allSolutions = [];
 
-  for(var rowsIndex = 0; rowsIndex < n; rowsIndex++) {
-    for(var colIndex = 0; colIndex < n; colIndex++) {
-      var board = this.findNRooksSolution(n, colIndex, rowsIndex);
-
-      // Check if solution is valid
-      var count = 0;
-      _.each(board, function(row) {
-        _.each(row, function(position) {
-          if (position === 1) {
-            count++;
-          }
-        });
-      });
-
-      console.table(board);
-
-      if (count === n) {
-        solutionCount++;
-        allSolutions.push(board);
-      }
-    }
-  }
+  this.findNRooksSolution(n, 0, 0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
